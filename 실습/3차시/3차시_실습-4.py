000a681397657c8241be0e5e0438ab745c156cbf
Gun Daniel Lee 이건 <gdlee412@gmail.com>
2022-09-27 03:04:45 +0900

    slight update

--- 실습/3차시/3차시_실습-4.py
+++ 실습/3차시/3차시_실습-4.py
@@ -4,9 +4,9 @@ scores = [70, 81, 65, 67, 65, 95, 97, 78, 82, 63, 73, 56, 77, 68, 74, 98, 84, 17
 min = int(input())
 max = int(input())
 
-ave = 0
+avg = 0
 
-for i in  range(min, max + 1):
-    ave += scores[i]
-ave = ave / (max - min + 1)
-print(ave)
+for i in range(min, max + 1):
+    avg += scores[i]
+avg = avg / (max - min + 1)
+print(avg)
