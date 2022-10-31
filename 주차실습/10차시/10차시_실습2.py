54118e5bf5aba1fca44817984f9d466301806773
Gun Daniel Lee 이건 <gdlee412@gmail.com>
2022-10-31 16:36:44 +0900

    Week 10 lecture

---
+++ 주차실습/10차시/10차시_실습2.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+os.makedirs('./data', exist_ok = True)
+fig_x = int(input())
+fig_y = int(input())
+fig = plt.figure(figsize=(fig_x, fig_y))
+
+## 코드 ##
+
+file_path = 'data/heiweimus.csv'
+df = pd.read_csv(file_path)
+
+plt.subplot(1,2,1)
+plt.scatter(df["height"], df["weight"])
+plt.title("height-weight")
+plt.subplot(1,2,2)
+plt.scatter(df["height"], df["muscle"])
+plt.title("height-muscle")
+
+##
+
+plt.savefig('./data/out.png')
+fig.canvas.draw()
+print(np.array(fig.canvas.renderer._renderer).sum(axis=(0, 1)))
