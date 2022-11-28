873ef0649f15328002afc1729f601b0921f15a98
Gun Daniel Lee 이건 <gdlee412@gmail.com>
2022-11-28 15:14:57 +0900

    Week 14 Lecture

---
+++ 주차실습/14차시/14차시_실습-1.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv('data/winequality-red.csv',dtype=float)
+
+
+#######################코드####################
+def encoding_quality(x):
+	if x > 5:
+		return 1
+	else:
+		return 0
+
+df['quality'] = df['quality'].apply(encoding_quality)
+#######################################
+
+print(len(df.loc[df['quality'] == 0]))
+print(len(df.loc[df['quality'] == 1]))
